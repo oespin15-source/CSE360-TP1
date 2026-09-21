2ea964402fef40494d899fe29cfd1a3bdfd5bffa
Noah-Les <nlesniew@asu.edu>
2026-09-21 08:30:00 -0700

    Implement One-Time Password setup method
    Replaced stub warning for One-Time Password with actual method call to display the password setup interface.

--- src/guiAdminHome/ControllerAdminHome.java
+++ src/guiAdminHome/ControllerAdminHome.java
@@ -111,11 +111,8 @@ public class ControllerAdminHome {
 	 * this function has not yet been implemented. </p>
 	 */
 	protected static void setOnetimePassword () {
-		System.out.println("\n*** WARNING ***: One-Time Password Not Yet Implemented");
-		ViewAdminHome.alertNotImplemented.setTitle("*** WARNING ***");
-		ViewAdminHome.alertNotImplemented.setHeaderText("One-Time Password Issue");
-		ViewAdminHome.alertNotImplemented.setContentText("One-Time Password Not Yet Implemented");
-		ViewAdminHome.alertNotImplemented.showAndWait();
+		guiSetOneTimePassword.ViewSetOneTimePassword.displaySetOneTimePassword(ViewAdminHome.theStage, 
+				ViewAdminHome.theUser);
 	}
 	
 	/**********
