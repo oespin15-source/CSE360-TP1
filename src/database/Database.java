373a3598ad207827adbe6b62f4e894230346f55b
Noah-Les <nlesniew@asu.edu>
2026-09-21 08:48:27 -0700

    Implement updatePassword method in Database class
    Added method to update user password in the database.

--- src/database/Database.java
+++ src/database/Database.java
@@ -541,6 +541,31 @@ public class Database {
 	    }
 		return;
 	}
+
+
+	/*******
+	 * <p> Method: void updatePassword(String username, String password) </p>
+	 * 
+	 * <p> Description: Update the password of a user given that user's username and the new
+	 * 		password.</p>
+	 * 
+	 * @param username is the username of the user
+	 *  
+	 * @param password is the new password for the user
+	 *  
+	 */
+	// update the password
+	public void updatePassword(String username, String password) {
+	    String query = "UPDATE userDB SET password = ? WHERE username = ?";
+	    try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+	        pstmt.setString(1, password);
+	        pstmt.setString(2, username);
+	        pstmt.executeUpdate();
+	        currentMiddleName = password;
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	}
 	
 	
 	/*******
