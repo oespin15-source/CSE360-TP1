3d8e69b4f97ea2595ec4753a1d08ffd66dd5d406
Noah-Les <nlesniew@asu.edu>
2026-09-22 07:16:06 -0700

    Add files via upload

---
+++ src/guiResetPassword/ModelResetPassword.java
@@ -0,0 +1,20 @@
+package guiResetPassword;
+
+/*******
+ * <p> Title: ModelSetOneTimePassword Class. </p>
+ * 
+ * <p> Description: The ResetPassword Page Model.  This class is not used as there is no
+ * data manipulated by this MVC beyond accepting role information and saving it in the
+ * database.</p>
+ * 
+ * <p> Copyright: Lynn Robert Carter © 2025 </p>
+ * 
+ * @author Lynn Robert Carter
+ * 
+ * @version 1.00		2025-08-15 Initial version
+ *  
+ */
+
+public class ModelResetPassword {
+
+}
